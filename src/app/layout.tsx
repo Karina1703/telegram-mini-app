f1d82d50316deb6e0974c1b9c535ed64d7635fdd
Karina Kabanova <karina.kabanova@jove.com>
2025-01-10 21:56:21 -0300

    test5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,10 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
+      <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
       </head>
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
